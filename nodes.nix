93efd15e62a1af1d700daa262c793b1be48d500d
hirohata <43802663+hitohata@users.noreply.github.com>
2026-02-05 09:20:39 -0800

    Updated the extra module (#24)

--- nodes.nix
+++ nodes.nix
@@ -5,16 +5,18 @@
     system = "x86_64-linux";
     hostname = "intel-pc";
     username = "root";
-    homeDir =  "root";
+    homeDir =  "/root";
     isNixos = false;
+    extraModules = [];
   };
   # for debugging
   "root@aarch64" = {
     system = "aarch64-linux";
     hostname = "aarch64";
     username = "root";
-    homeDir =  "root";
+    homeDir =  "/root";
     isNixos = false;
+    extraModules = [];
   };
 
   # general user
@@ -24,6 +26,7 @@
     username = "hoge";
     homeDir =  "/home/hoge";
     isNixos = false;
+    extraModules = [];
   };
 
   "user@n100" = {
@@ -32,5 +35,6 @@
     username = "hoge";
     homeDir =  "/home/hoge";
     isNixos = true;
+    extraModules = [];
   };
 }
