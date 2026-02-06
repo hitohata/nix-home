4c1d36817556f0f17e6e69340a4859cfe4abd53f
hirohata <hirohatatro@gmail.com>
2026-02-05 21:03:28 -0800

    Add markdown support plugins and update manual
    Plugins added:
    - render-markdown.nvim: Enhanced markdown rendering
    - zen-mode.nvim: Distraction-free writing mode
    - markdown-preview.nvim: Live preview in browser
    
    Manual updates:
    - Added Markdown section with keybindings
    - Added Commands section with custom and built-in commands
    - Keybindings: <leader>mr (toggle render), <leader>z (zen mode), <leader>mp (preview)
    
    Resolves #31

--- modules/nvim/plugins/languages.nix
+++ modules/nvim/plugins/languages.nix
@@ -11,5 +11,47 @@
       }
     '';
   }
+
+  # Markdown rendering
+  {
+    plugin = render-markdown-nvim;
+    type = "lua";
+    config = ''
+      require("render-markdown").setup {
+        file_types = { "markdown" },
+        render_modes = { "n", "c" },
+      }
+      vim.keymap.set("n", "<leader>mr", "<cmd>RenderMarkdown toggle<cr>", { desc = "Toggle markdown render" })
+    '';
+  }
+
+  # Zen mode for focused writing
+  {
+    plugin = zen-mode-nvim;
+    type = "lua";
+    config = ''
+      require("zen-mode").setup {
+        window = {
+          width = 0.85,
+          options = {
+            number = false,
+            relativenumber = false,
+          },
+        },
+      }
+      vim.keymap.set("n", "<leader>z", "<cmd>ZenMode<cr>", { desc = "Toggle zen mode" })
+    '';
+  }
+
+  # Markdown preview in browser
+  {
+    plugin = markdown-preview-nvim;
+    type = "lua";
+    config = ''
+      vim.g.mkdp_auto_close = 0
+      vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreview<cr>", { desc = "Markdown preview" })
+      vim.keymap.set("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", { desc = "Stop markdown preview" })
+    '';
+  }
 ]
 
