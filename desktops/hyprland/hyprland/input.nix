32f681a28adb5a18e8d071879c2fb52e90bc9485
hirohata <hirohatatro@gmail.com>
2026-02-08 18:13:44 -0800

    feat: updated the input

--- desktops/hyprland/hyprland/input.nix
+++ desktops/hyprland/hyprland/input.nix
@@ -18,11 +18,11 @@
   };
 
   gestures = {
-    workspace_swipe = true;
     workspace_swipe_fingers = 3;
     workspace_swipe_distance = 300;
     workspace_swipe_touch = false;
-    workspace_swipe_numbered = false;
-    workspace_swipe_forever = false;
+    workspace_swipe_cancel_ratio = 0.5;
+    workspace_swipe_min_speed_to_force = 30;
+    workspace_swipe_create_new = true;
   };
 }
