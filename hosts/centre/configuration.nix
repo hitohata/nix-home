f84343703dce4f628a7420613dbf5e5106bf56bb
hitohata <hirohatatro@gmail.com>
2026-02-28 14:32:38 -0800

    feat: added git

--- hosts/centre/configuration.nix
+++ hosts/centre/configuration.nix
@@ -36,7 +36,9 @@
     isNormalUser = true;
     description = "centre";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
+    packages = with pkgs; [
+      git
+    ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPecawIGB5QnbVGj1g0My61YdryyuAVysqu2r87tND1J m3"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIrxRnkBpffDfzvAiNkkpRA3jIMfEiZQmAJW9WoCjwV node"
