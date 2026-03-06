709f17a53969a853d08c0bacb2c64f3d3d666a12
hirohata <43802663+hitohata@users.noreply.github.com>
2026-03-05 17:36:29 -0800

    Mount NAS setting (#81)

---
+++ hosts/shared/nas.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+{
+  services.rpcbind.enable = true;
+  boot.supportedFilesystems = [ "nfs" ];
+  fileSystems."mnt/pi_nas" = {
+    device = "pi-nas.local:/Pi-NAS";
+    fsType = "nfs";
+    options = [
+      "nfsvers=4"
+      "rw"
+      "x-systemd.automount" # auto mount when it needs
+      "noauto" # prevent stopping by an error when the network is broken
+      "x-systemd.idle-timeout=300" # 5 min
+    ];
+  };
+}
