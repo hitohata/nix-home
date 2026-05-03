61e5f5821adc2720219e5106ca836169de887e51
hitohata <hirohatatro@gmail.com>
2026-05-03 16:25:59 -0700

    feat: added a new kew

--- hosts/n100/configuration.nix
+++ hosts/n100/configuration.nix
@@ -53,6 +53,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPecawIGB5QnbVGj1g0My61YdryyuAVysqu2r87tND1J m3"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIrxRnkBpffDfzvAiNkkpRA3jIMfEiZQmAJW9WoCjwV node"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINEzr66JjoWf5GhDxiaPWx7wz113IgZFQTIt9jtllRvW x1"
     ];
   };
 
