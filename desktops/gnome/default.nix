95cbbbfae78f6a5b618505b0990d343f824a58fc
hitohata <hirohatatro@gmail.com>
2026-06-11 08:33:48 -0700

    fix: gnome setting

--- desktops/gnome/default.nix
+++ desktops/gnome/default.nix
@@ -2,11 +2,11 @@
 
 {
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.desktopManager.gnome.enable = true;
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
 }
