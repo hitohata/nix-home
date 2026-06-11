6c63244f8ee17a493a89dbc3e21e89ca3eb4ac69
hitohata <hirohatatro@gmail.com>
2026-06-10 21:44:20 -0700

    fix: input method

--- hosts/shared/input.nix
+++ hosts/shared/input.nix
@@ -2,9 +2,17 @@
   i18n = {
     defaultLocale = "en_CA.UTF-8";
     inputMethod = {
-      enabled = "ibus";
+      enable = true;
+      type = "ibus";
       ibus.engines = with pkgs.ibus-engines; [ mozc ];
     };
+  };
+
+  # for Wayland
+  environment.variables = {
+    QT_IM_MODULE = "ibus";
+    XMODIFIERS = "@im=ibus";
+    GTK_IM_MODULE = "ibus";
   };
 
   fonts.packages = with pkgs; [
