c035bf18066db837851ee0bb0f3ae6afbee5ac4a
hitohata <hitohatatro@gmail.com>
2026-06-13 15:40:50 -0700

    feat: swap the immich server

--- hosts/centre/configuration.nix
+++ hosts/centre/configuration.nix
@@ -10,7 +10,7 @@
       ./hardware-configuration.nix
       ../shared/nas.nix
       ../shared/avahi.nix
-      ../../services/immich.nix
+      # ../../services/immich.nix
       ../../services/nextcloud.nix
     ];
 
