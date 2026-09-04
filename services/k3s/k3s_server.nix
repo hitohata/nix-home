f7c7e06b48b2791fb253759d972e350240b99e29
hitohata <hitohatatro@gmail.com>
2026-09-04 07:46:10 -0700

    feat: updated the k3s setting

--- services/k3s/k3s_server.nix
+++ services/k3s/k3s_server.nix
@@ -7,10 +7,16 @@
     enable = true;
     role = "server";
     extraFlags = [
+      # host name
       "--tls-san ${config.networking.hostName}"
       "--tls-san ${config.networking.hostName}.lan"
       "--tls-san ${config.networking.hostName}.local"
+      # IP
+      "--tls-san ${config.services.dejimaClient.ipAddress}" # ← ip
+
       "--node-name ${config.networking.hostName}"
+      "--node-ip ${config.services.dejimaClient.ipAddress}"
+
       "--write-kubeconfig-mode=0644"
     ];
   };
